3a7043ec353bb7c4fb8cee46a46a44d84a66b535
Doug Gerber <dojoh@outlook.com>
2013-09-06 17:33:16 -0400

    Clean up

--- array_stats.rb
+++ array_stats.rb
@@ -87,3 +87,46 @@ end
 # a = ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500, 5500])
 
 # a.freq
+
+describe ArrayStats do 
+	it 'returns the largest number in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).largest).to eql(5500)
+	end
+
+	it 'returns the smallest number in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).smallest).to eql(1)
+	end
+
+	it 'returns the average number in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).average).to eql(2000)
+	end
+
+	it 'returns the standard deviation in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).standard_dev).to eql(2175)
+	end
+
+	it 'returns the nth smallest number in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).nth_number(3)).to eql(1000)
+	end
+
+	it 'returns the sum number in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).sum).to eql(14001)
+	end
+
+	it 'returns the product in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).product).to eql(3960000000000000000)
+	end
+
+	it 'returns the difference between the highest and lowest values in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).difference).to eql(5499)
+	end
+
+	it 'returns a hash that breaks down how many times n-digit numbers are provided in the array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500]).freq).to eql({ '1' => 1, '3' => 2, '4' => 4 })
+	end
+
+	it 'returns a hash that breaks down how many times n-digit numbers are in the provided array' do
+			expect(ArrayStats.new([1, 1000, 200, 300, 3000, 4000, 5500, 5500]).digit_freq).to eql({1 => 1, 200 => 1, 300 => 1, 1000 => 1, 3000 => 1, 4000 => 1, 5500 => 2})
+	end
+end
+
